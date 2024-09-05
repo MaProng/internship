ca3e11e6db8e0704dea7eac6869c044f7dc8d804
MaProng <pohnsuda40249@gmail.com>
2024-09-04 22:04:38 -0700

    Adding a reporting tool

--- features/environment.py
+++ features/environment.py
@@ -15,9 +15,9 @@ def browser_init(context):
 def browser_init(context, scenario_name):
 
     ## CHROM BROWSER ##
-    # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Chrome(service=service)
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
 
 
     ## FIREFOX BROWSER ##
@@ -35,19 +35,19 @@ def browser_init(context, scenario_name):
 
     ### BROWSERSTACK ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-    bs_user = 'prongks_7DHLoc'
-    bs_key = 'sSgGWxBZxUmHgbRpBjzJ'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-
-    options = Options()
-    bstack_options = {
-        "os": "Windows",
-        "osVersion": "11",
-        'browserName': 'edge',
-        'sessionName': scenario_name
-    }
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
+    # bs_user = 'prongks_7DHLoc'
+    # bs_key = 'sSgGWxBZxUmHgbRpBjzJ'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     "os": "Windows",
+    #     "osVersion": "11",
+    #     'browserName': 'edge',
+    #     'sessionName': scenario_name
+    # }
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
 
     context.driver.maximize_window()
